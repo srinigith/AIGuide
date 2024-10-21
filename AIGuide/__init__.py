0d0d553c6d8628ae223df0f0770952d9468328cc
srinigith <srinigith@gmail.com>
2024-10-21 10:46:14 +0530

    Update __init__.py

--- AIGuide/__init__.py
+++ AIGuide/__init__.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 app.config.from_object(config("APP_SETTINGS"))
 # app.config.update(
 #     #TESTING=True,
-#     GROQ_API_KEY="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"
 # )
 
 import AIGuide.views
